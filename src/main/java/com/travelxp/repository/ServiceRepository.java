301785d8aa8f24e84bc4280d85030c5e72569d13
Yassine Raddadi <yassine.raddadi.4@gmail.com>
2026-02-20 17:45:52 +0100

    deleted:    All

--- src/main/java/com/travelxp/repository/ServiceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.travelxp.repository;
-
-import com.travelxp.model.Service;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceRepository extends JpaRepository<Service, Long> {
-}
